7c404e302b8ba62a3bc5395c0f2eeab03e2c72e5
Christoph Kolb <christoph@kolbgrafik.de>
2018-10-11 21:38:15 +0200

    fuck me sideways

--- index.js
+++ index.js
@@ -41,11 +41,7 @@ var AuroraRequest = module.exports = function(opts) {
             reject(res.statusCode);
           }
         } else if (method === "PUT") {
-          if (res !== undefined && res.statusCode === 204) {
-            resolve(res.statusCode);
-          } else {
-            reject(res.statusCode);
-          }
+          resolve(res);
         }
       })
     })
